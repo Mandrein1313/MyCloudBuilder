dc63be88e3d58374f56989861307fe3ae275e7b1
Mandrein <145546234+Mandrein1313@users.noreply.github.com>
2026-02-12 18:45:04 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.myapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
